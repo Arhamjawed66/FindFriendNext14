b71c2f4d3d228a854b00493b868cb599044265eb
jawed shiekh <abduljawed@gmail.com>
2024-12-04 21:46:36 +0500

    ComponentsSet

--- src/app/(admin)/admin/event/page.js
+++ src/app/(admin)/admin/event/page.js
@@ -6,7 +6,7 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-  } from "@/components/ui/table"
+  } from "@/src/app/components/ui/table"
 import Image from "next/image"
 
 
---
+++ src/app/components/loading/skeleton.js
@@ -0,0 +1,16 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+
+export default function Skeleton({
+  className,
+  ...props
+}) {
+  return (
+    (<div
+      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      {...props} />)
+  );
+}
+
+export { Skeleton }
